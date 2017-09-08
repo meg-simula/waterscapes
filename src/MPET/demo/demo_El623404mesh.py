ce82283892c5ce625f82055daae11caaaa802a80
elepiersan <piersanti.eleonora@gmail.com>
2017-09-07 22:56:40 -0700

    removed pytest from demo_El623404mesh.py

--- src/MPET/demo/demo_El623404mesh.py
+++ src/MPET/demo/demo_El623404mesh.py
@@ -1,7 +1,7 @@
 __author__ = "Eleonora Piersanti (eleonora@simula.no) and Marie E. Rognes (meg@simula.no), 2017"
 
 import math
-import pytest
+# import pytest
 from mpet import *
 from matplotlib import pylab
 from datetime import datetime
@@ -211,3 +211,4 @@ if __name__ == "__main__":
 
     biot_El623404()
 
+# 
