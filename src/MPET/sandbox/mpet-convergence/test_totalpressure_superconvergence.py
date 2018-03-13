f4b11152899e54d4f8fc521e80cb1e3d61146777
Marie E. Rognes <meg@simula.no>
2018-03-13 12:07:10 +0100

    Update superconvergence code in accordance with previous updates. Results as expected now.

--- src/MPET/sandbox/mpet-convergence/test_totalpressure_superconvergence.py
+++ src/MPET/sandbox/mpet-convergence/test_totalpressure_superconvergence.py
@@ -10,94 +10,7 @@ parameters["form_compiler"]["cpp_optimize"] = True
 flags = ["-O3", "-ffast-math", "-march=native"]
 parameters["form_compiler"]["cpp_optimize_flags"] = " ".join(flags)
 
-def convergence_rates(errors, hs):
-    import math
-    rates = [(math.log(errors[i+1]/errors[i]))/(math.log(hs[i+1]/hs[i]))
-             for i in range(len(hs)-1)]
-    return rates
-
-def exact_solutions(params):
-    import math
-    import sympy
-
-    A = params["A"]
-    nu = params["nu"]
-    E = params["E"]
-    alpha = params["alpha"]
-    c = params["c"]
-    K = params["K"]
-    S = params["S"]
-    
-    # I is short-hand for range(d):
-    gdim = 2
-    I = range(gdim)
-    As = range(A)
-    
-    # Convert (nu, E) to (mu, labda)
-    lmbda = nu*E/((1.0-2.0*nu)*(1.0+nu))
-    mu = E/(2.0*(1.0+nu))
-    print "(mu, lmbda) = ", (mu, lmbda)
-
-    # Sympy utensils
-    pi = math.pi
-    omega = 2*pi
-    sin = sympy.sin
-    cos = sympy.cos
-    diff = sympy.diff
-    
-    x = sympy.symbols(("x[0]", "x[1]"))
-    t = sympy.symbols("t")
-
-    # Define exact solutions u and p
-    u = [(sin(2*pi*x[1])*(-1.0 + cos(2*pi*x[0])) + 1.0/(mu + lmbda)*sin(pi*x[0])*sin(pi*x[1]))*t,
-         (sin(2*pi*x[0])*(1.0 - cos(2*pi*x[1])) + 1.0/(mu + lmbda)*sin(pi*x[0])*sin(pi*x[1]))*t]
-    p = [-(a+1)*sin(pi*x[0])*sin(pi*x[1])*t for a in As]
-        
-    div_u = sum([diff(u[i], x[i]) for i in I])
-
-    # Define total pressure p0
-    p0 = lmbda*div_u - sum([alpha[a]*p[a] for a in As])
-
-    # Simplify symbolics 
-    u = [sympy.simplify(u_i) for u_i in u]
-    p = [sympy.simplify(p_i) for p_i in p]
-    p0 = sympy.simplify(p0)
-    
-    # Compute sigma_ast
-    grad_u = [[diff(u[i], x[j]) for j in I] for i in I]
-    eps_u = [[0.5*(grad_u[i][j] + grad_u[j][i]) for j in I] for i in I]
-    grad_p = [[diff(p[a], x[j]) for j in I] for a in As]
-    sigma_ast = [[2*mu*eps_u[i][j] for j in I] for i in I]
-
-    # Compute f (vector of length gdim)
-    div_sigma_ast = [sum([diff(sigma_ast[i][j], x[j]) for j in I]) for i in I]
-    f = [- (div_sigma_ast[j] + diff(p0, x[j])) for j in I]
-    f = [sympy.simplify(f_i) for f_i in f]
-
-    # Compute g (vector of length A)
-    g = [None for i in range(A)]
-
-    divu_t = diff((p[0] + sum([alpha[b]*p[b] for b in As])), t)/lmbda
-    for a in As:
-        g[a] = - c[a]*diff(p[a], t) \
-               - alpha[a]*divu_t \
-               + sum([diff(K[a]*grad_p[a][j], x[j]) for j in I]) \
-               - sum(S[a][b]*(p[a] - p[b]) for b in range(A))
-    g = [sympy.simplify(gi) for gi in g]
-    
-    # Print sympy expressions as c++ code
-    u_str = [sympy.printing.ccode(u[i]) for i in I]
-    p_str = [sympy.printing.ccode(p[a]) for a in As]
-    p0_str = sympy.printing.ccode(p0)
-    f_str = [sympy.printing.ccode(f[i]) for i in I]
-    g_str = [sympy.printing.ccode(g[a]) for a in As]
-
-    print "Exact u = ", u_str
-    print "Exact p_0 = ", p0_str
-    for a in As:
-        print "Exact p_%d = %s" % (a+1, p_str[a])
-
-    return (u_str, p_str, p0_str, f_str, g_str)
+from test_mpetsolver_semidiscrete import exact_solutions, convergence_rates
 
 def elliptic_interpolant(K_j, p_j, mesh, l_j):
 
@@ -121,7 +34,7 @@ def single_run(n=8, M=8, theta=1.0):
     "N is t_he mesh size, M the number of time steps."
     
     # Define end time T and timestep dt
-    T = 1.0
+    T = 0.5
     dt = float(T/M)
 
     # Define material parameters in MPET equations
@@ -135,7 +48,7 @@ def single_run(n=8, M=8, theta=1.0):
     params = dict(A=A, alpha=alpha, K=K, S=S, c=c, nu=nu, E=E)
 
     info("Deriving exact solutions")
-    u_e, p_e, p0_e, f, g = exact_solutions(params)
+    u_e, p_e, f, g, p0_e = exact_solutions(params)
 
     info("Setting up MPET problem")
     mesh = UnitSquareMesh(n, n)
@@ -194,7 +107,7 @@ def convergence_exp(theta):
     import time
     
     # Remove all output from FEniCS (except errors)
-    set_log_level(ERROR)
+    set_log_level(LogLevel.ERROR)
 
     # Make containers for errors
     u_errorsL2 = []
@@ -213,7 +126,7 @@ def convergence_exp(theta):
     ms = [4, ]*len(ns)
 
     for (n, m) in zip(ns, ms):
-        print "(n, m) = ", (n, m)
+        print("(n, m) = ", (n, m))
         errors = single_run(n, m, theta)
         (erruL2, erruH1, errpL2, errpH1, h) = errors["e"]
         (erruhL2, erruhH1, errphL2, errphH1, h) = errors["e_h"]
@@ -234,25 +147,25 @@ def convergence_exp(theta):
             ph_errorsH1[i] += [errpi]
 
             
-    print "u_errorsL2 = ", ["%0.2e" % i for i in u_errorsL2]
-    print "u_errorsH1 = ", ["%0.2e" % i for i in u_errorsH1]
+    print("u_errorsL2 = ", ["%0.2e" % i for i in u_errorsL2])
+    print("u_errorsH1 = ", ["%0.2e" % i for i in u_errorsH1])
     for a in range(3):
-        print "p[%d]_errorsL2 = " % (a), ["%0.2e" % i for i in p_errorsL2[a]]
-        print "p[%d]_errorsH1 = " % (a), ["%0.2e" % i for i in p_errorsH1[a]]
+        print("p[%d]_errorsL2 = " % (a), ["%0.2e" % i for i in p_errorsL2[a]])
+        print("p[%d]_errorsH1 = " % (a), ["%0.2e" % i for i in p_errorsH1[a]])
 
-    print
+    print("")
 
     for a in range(3):
-        print "p[%d]_errorsL2 = " % (a), ["%0.2e" % i for i in p_errorsL2[a]]
-        print "p[%d]_errorsH1 = " % (a), ["%0.2e" % i for i in p_errorsH1[a]]
+        print("p[%d]_errorsL2 = " % (a), ["%0.2e" % i for i in p_errorsL2[a]])
+        print("p[%d]_errorsH1 = " % (a), ["%0.2e" % i for i in p_errorsH1[a]])
 
-    print
+    print("")
 
     for a in range(2):
-        print "ph[%d]_errorsL2 = " % (a), ["%0.2e" % i for i in ph_errorsL2[a]]
-        print "ph[%d]_errorsH1 = " % (a), ["%0.2e" % i for i in ph_errorsH1[a]]
+        print("ph[%d]_errorsL2 = " % (a), ["%0.2e" % i for i in ph_errorsL2[a]])
+        print("ph[%d]_errorsH1 = " % (a), ["%0.2e" % i for i in ph_errorsH1[a]])
 
-    print
+    print("")
     
     # Compute convergence rates:
     u_ratesL2 = convergence_rates(u_errorsL2, hs)
@@ -269,26 +182,25 @@ def convergence_exp(theta):
     ph1_ratesH1 = convergence_rates(ph_errorsH1[0], hs)
     ph2_ratesH1 = convergence_rates(ph_errorsH1[1], hs)
 
-    print "u_ratesL2 = ", ["%0.2f" % i for i in u_ratesL2]
-    print "u_ratesH1 = ", ["%0.2f" % i for i in u_ratesH1]
-    print "p0_ratesL2 = ", ["%0.2f" % i for i in p0_ratesL2]
-    print "p1_ratesL2 = ", ["%0.2f" % i for i in p1_ratesL2]
-    print "p2_ratesL2 = ", ["%0.2f" % i for i in p2_ratesL2]
-    print "p0_ratesH1 = ", ["%0.2f" % i for i in p0_ratesH1]
-    print "p1_ratesH1 = ", ["%0.2f" % i for i in p1_ratesH1]
-    print "p2_ratesH1 = ", ["%0.2f" % i for i in p2_ratesH1]
-    print
-    print "ph1_ratesL2 = ", ["%0.2f" % i for i in ph1_ratesL2]
-    print "ph2_ratesL2 = ", ["%0.2f" % i for i in ph2_ratesL2]
-    print "ph1_ratesH1 = ", ["%0.2f" % i for i in ph1_ratesH1]
-    print "ph2_ratesH1 = ", ["%0.2f" % i for i in ph2_ratesH1]
+    print("u_ratesL2 = ", ["%0.2f" % i for i in u_ratesL2])
+    print("u_ratesH1 = ", ["%0.2f" % i for i in u_ratesH1])
+    print("p0_ratesL2 = ", ["%0.2f" % i for i in p0_ratesL2])
+    print("p1_ratesL2 = ", ["%0.2f" % i for i in p1_ratesL2])
+    print("p2_ratesL2 = ", ["%0.2f" % i for i in p2_ratesL2])
+    print("p1_ratesH1 = ", ["%0.2f" % i for i in p1_ratesH1])
+    print("p2_ratesH1 = ", ["%0.2f" % i for i in p2_ratesH1])
+    print("")
+    print("ph1_ratesL2 = ", ["%0.2f" % i for i in ph1_ratesL2])
+    print("ph2_ratesL2 = ", ["%0.2f" % i for i in ph2_ratesL2])
+    print("ph1_ratesH1 = ", ["%0.2f" % i for i in ph1_ratesH1])
+    print("ph2_ratesH1 = ", ["%0.2f" % i for i in ph2_ratesH1])
     
     end = time.time()
-    print "Time_elapsed = ", end - start
+    print("Time_elapsed = ", end - start)
 
 def test_convergence():
-    #convergence_exp(0.5)
-    convergence_exp(1.0)
+    convergence_exp(0.5)
+    #convergence_exp(1.0)
 
 if __name__ == "__main__":
 
